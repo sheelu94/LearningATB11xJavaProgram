d26adfb9375fb573299b532ec571aa268cc0b513
Sheelu Sharma <krishnasheelu1994@gmail.com>
2025-03-07 00:34:32 +0530

    6 March  2 file again committing code

--- src/Lab01_Java_Baisc/HelloWorld.java
+++ src/Lab01_Java_Baisc/HelloWorld.java
@@ -3,5 +3,6 @@ package Lab01_Java_Baisc;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Hi");
     }
 }
