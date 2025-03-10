f9fbbff7d6106ead6029db57bd651567be43a1bd
Sheelu Sharma <krishnasheelu1994@gmail.com>
2025-03-10 08:35:57 +0530

    10 March  multiple println used

---
+++ src/Lab01_Java_Baisc/Comments_Java.java
@@ -0,0 +1,13 @@
+package Lab01_Java_Baisc;
+
+public class Comments_Java {
+    public static void main(String[] args) {
+        System.out.println("Here we are using single line comments");
+//         the above line will print the single line
+        System.out.println("now we will use multi line comments");
+        /*this is
+        multi line comments
+        which will ingored by java
+         */
+    }
+}
