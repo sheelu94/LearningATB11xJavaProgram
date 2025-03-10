f66cc3fd1300ee03d5d4ff721ad4e1896f5a989d
Sheelu Sharma <krishnasheelu1994@gmail.com>
2025-03-10 22:56:59 +0530

    10 March  multiple println used

---
+++ src/Lab01_Java_Baisc/Air_Exception.java
@@ -0,0 +1,7 @@
+package Lab01_Java_Baisc;
+
+public class Air_Exception {
+    public static void main(String[] args) {
+        System.out.println(10/0); // Exception division by zero//
+    }
+}
--- src/Lab01_Java_Baisc/Lab03_Java_Basic.java
+++ src/Lab01_Java_Baisc/Lab03_Java_Basic.java
@@ -3,10 +3,10 @@ package Lab01_Java_Baisc;
 public class Lab03_Java_Basic {
     public static void main(String[] args) {
         System.out.println(2+2);
-        System.out.println(30-10);
-        System.out.println(30*30);
+        System.out.println("Substraction of two number is :" + (30-10));
+        System.out.println("multiply of two number is :" + (30*30));
         System.out.println("i am sheelu sharma");
-        System.out.print("i am from indore");
+        System.out.print("i am from indore"); // println vs Print ( println- new line)//
         System.out.println("having 5 yrs of exp");
         System.out.println("in manual and api testing!  ");
     }
