cbc5acafbc637d5d4f33bff812f68d35d6177dab
Sheelu Sharma <krishnasheelu1994@gmail.com>
2025-03-10 08:30:07 +0530

    10 March  multiple println used

---
+++ src/Lab01_Java_Baisc/Lab03_Java_Basic.java
@@ -0,0 +1,13 @@
+package Lab01_Java_Baisc;
+
+public class Lab03_Java_Basic {
+    public static void main(String[] args) {
+        System.out.println(2+2);
+        System.out.println(30-10);
+        System.out.println(30*30);
+        System.out.println("i am sheelu sharma");
+        System.out.print("i am from indore");
+        System.out.println("having 5 yrs of exp");
+        System.out.println("in manual and api testing!  ");
+    }
+}
